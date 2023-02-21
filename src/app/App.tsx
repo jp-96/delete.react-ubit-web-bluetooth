c816926cd3c37c4ff6a4b1cec80e258c42b8ecd7
jp-96 <jp-96@outlook.jp>
2023-02-21 07:46:22 +0000

    rename state and event

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,8 +1,8 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import Logo from './Logo';  // logo.svg ==> Log0.tsx
 //import './App.css'; // ==> ../index.html
 //import { useMicrobitBLE } from './hooks/MicrobitBLE';
-import { Services } from 'microbit-web-bluetooth';
+//import { Services } from 'microbit-web-bluetooth';
 import { useBindServicesCallback, useContextActor } from './StateMachine/MicrobitBluetoothMachineContext';
 
 function App() {
@@ -31,6 +31,8 @@ function App() {
 
   const [state, send] = useContextActor();
 
+  const listItems = state.context.microbitServices ? Object.keys(state.context.microbitServices).map((serviceName) => <li>{serviceName}</li> ) : undefined;
+
   return (
     <div className="App">
       <header className="App-header">
@@ -47,15 +49,21 @@ function App() {
           Learn React
         </a>
         <p>
+          {"[" + state.toStrings() + "]"}<br/>
           <button onClick={() => send("RESET")}>RESET</button>
           <button onClick={() => send("REQUEST")}>REQUEST</button>
           <button onClick={() => send("CONNECT")}>Connect</button>
           <button onClick={() => send("DISCONNECT")}>Disconnect</button>
         </p>
-        <p>{state.toStrings()}</p>
         <p>
+          {state.context.microbitDevice?.name && ("[" + state.context.microbitDevice.name + "]")}<br/>
           Button A: {`${buttonA}`}<br/>
           Button B: {`${buttonB}`}
+        </p>
+        {listItems && <p>services:<ul>{listItems}</ul></p>}
+        <p>
+          {state.context.rejectedReason && ("rejected: " + state.context.rejectedReason)}<br/>
+          {state.context.disconnectedReason && ( "disconnected: " + state.context.disconnectedReason)}
         </p>
       </header>
     </div>
