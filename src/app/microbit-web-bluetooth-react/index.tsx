4d30318865d1338160d6fbd886edf3eb0aa08e85
jp-96 <jp-96@outlook.jp>
2023-02-28 01:24:57 +0000

    remove Services

--- src/app/microbit-web-bluetooth-react/index.tsx
+++ src/app/microbit-web-bluetooth-react/index.tsx
@@ -1,7 +1,6 @@
 import { MicrobitContextProvider } from './src/context/Microbit';
 export default MicrobitContextProvider;
 
-export { Services } from 'microbit-web-bluetooth';
 export { DeviceEffector, ServicesEffector, useMicrobitActor } from './src/context/Microbit';
 export { MicrobitDevice } from './src/context/MicroBitDevice';
 export { MicrobitServices } from './src/context/MicroBitServices';
